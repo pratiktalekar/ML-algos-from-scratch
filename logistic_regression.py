60386f660b866bef4a33ec0d2036cf2901b3e1e4
pratik talekar <pratiktalekarpatil@gmail.com>
2024-09-12 15:34:04 +0530

    created logistic_regression.py

---
+++ logistic_regression.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class logistic_regression() :
+
+    def __init__(self, learning_rate, iterations) :
+        self.lr = learning_rate
+        self.iterations = iterations
+
+    def sigmoid(self, x) :
+        return 1/(1+ np.exp(-x))
+
+    def fit(self, X, y) :
+        n_samples, n_features = np.shape
+        self.weights = np.zeros(n_features)
+        self.bias = bias
+
+        for i in range(self.iterations) :
+            
+            linear_data = np.dot(X, self.weights) + self.bias
+            y_pred = self.sigmoid(linear_data)
+
+            dw = (1/n_samples) * np.dot(X.T, (y-y_pred))
+            db = (1/n_samples) * np.dot(x.T, (y - y_pred))
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+    
+    def predict(self, X) :
+        linear_data = np.dot(X.T, self.weights) + self.bias
+        y = self.sigmoid(linear_data)
+        y_class = [1 if i>0.5 else 0 for i in linear_data]
+        return np.array(y_class)
+    
+
+        
+            
+        
