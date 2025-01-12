19a298b576750050e7f68e0722dfcb64ca56bbd8
pratik <pratiktalekarpatil@gmail.com>
2025-01-12 16:43:33 +0530

    linear regression normal equation changed

--- linear regression/linear_regression.py
+++ linear regression/linear_regression.py
@@ -18,10 +18,10 @@ class regression(object) :
 
     def fit(self, X, y) :
         # insert ones for bias weights
-        X = np.insert(X, 0, 1, axis=1)
+        X = np.insert(X, 0, np.ones(X.shape[0], 1), axis=1)
         self.initializing_weights(X.shape[1])
 
-        for i in range(self.n_iterations) :
+        for i in range(self.n_iterations):
             y_pred = X.dot(self.w)
             mse = 0.5 * (y - y_pred)**2
 
@@ -34,30 +34,24 @@ class regression(object) :
         y_pred = X.dot(self.w)
         return y_pred
     
-class normal_equation() :
+class normal_equation(regression):
+    def __init__(self, n_iterations, learning_rate, gradient_descent=True):
+        super().__init__(n_iterations, learning_rate)
+        self.gradient_descent = gradient_descent
+        self.w = None
 
     def fit(self, X, y):
-            
-        # Insert constant ones for bias weights
-        X = np.insert(X, 0, 1, axis=1)
-        # Calculate weights by least squares (using Moore-Penrose pseudoinverse)
-        U, S, V = np.linalg.svd(X.T.dot(X))     
-        S = np.diag(S)
-        X_sq_reg_inv = V.dot(np.linalg.pinv(S)).dot(U.T)
-        self.w = X_sq_reg_inv.dot(X.T).dot(y)
+        if not self.gradient_descent:
+            X_new = np.c_(np.ones(X.shape[0], 1), X)
+            self.initializing_weights(X_new.shape[1])
+            self.w = np.linalg.inv(X_new.T.dot(X_new)).dot(X_new.T).dot(y)
+        else:
+            super().fit(self, X, y)
 
-    def predict1(self, X, y) :
-        X = np.insert(X, 0, 1, axis=1)
-        y_pred = X.dot(self.w)
+    def predict(self, X):
+        X_new = np.c_(np.ones(1, 1), X)
+        y_pred = self.w * X_new
         return y_pred
-    
-
-    
-
-
-
-
-
 '''
 1. np.insert(arr, obj, values, axis) 
     arr is in which we have to insert the values, obj is index before which we have to insert values
